db86a4ecc38785a15c2bb02bc6f669755377d8cd
Paolo de Bruyn <mailingpaolo@gmail.com>
2020-09-16 16:04:32 +0200

    LED test

---
+++ LED_test.py
@@ -0,0 +1,9 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(14, GPIO.OUT)
+GPIO.output(14, GPIO.HIGH)
+time.sleep(2)
+GPIO.output(14, GPIO.LOW)
